d06e2a686c17fdcfc5274e04f8217c6d28a59a62
Andrea Spiga <drespriz@gmail.com>
2022-05-19 14:43:29 +0200

    creates controllers for lists, movies, bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/new'
+  get 'lists/show'
+  get 'lists/create'
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
